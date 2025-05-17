df833e12133f9a7a0f02272ac3c637f6d6c9814f
Victor <abysswalk3r69@gmail.com>
2025-05-17 22:48:59 +0300

    Rewritten to Migration
    SQLite doesn't support AsyncMigration

--- Sources/ACBackend/Migrations/CreateNote.swift
+++ Sources/ACBackend/Migrations/CreateNote.swift
@@ -1,14 +1,26 @@
 import Fluent
 
-struct CreateNote: AsyncMigration {
-    func prepare(on database: any Database) async throws {
-        try await database.schema(Note.schema)
+struct CreateNote: Migration {
+    func prepare(on database: any Database) -> EventLoopFuture<Void> {
+        database.schema(Note.schema)
             .id()
             .field("name", .string, .required)
             .create()
     }
 
-    func revert(on database: any Database) async throws {
-        try await database.schema(Note.schema).delete()
+    func revert(on database: any Database) -> EventLoopFuture<Void> {
+        database.schema(Note.schema).delete()
     }
 }
+//struct CreateNote: AsyncMigration {
+//    func prepare(on database: any Database) async throws {
+//        try await database.schema(Note.schema)
+//            .id()
+//            .field("name", .string, .required)
+//            .create()
+//    }
+//
+//    func revert(on database: any Database) async throws {
+//        try await database.schema(Note.schema).delete()
+//    }
+//}
