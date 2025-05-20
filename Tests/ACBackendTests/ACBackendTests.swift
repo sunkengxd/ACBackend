01bfa7e0dd2ff32364cb8fdc73b4e244d32cd51d
Victor <abysswalk3r69@gmail.com>
2025-05-20 16:45:50 +0300

    Naming

--- Tests/ACBackendTests/ACBackendTests.swift
+++ Tests/ACBackendTests/ACBackendTests.swift
@@ -33,41 +33,41 @@ struct ACBackendTests {
     @Test("Getting all the Todos")
     func getAllTodos() async throws {
         try await withApp { app in
-            let sampleTodos = [Todo(title: "sample1"), Todo(title: "sample2")]
+            let sampleTodos = [Note(title: "sample1"), Note(title: "sample2")]
             try await sampleTodos.create(on: app.db)
             
             try await app.testing().test(.GET, "todos", afterResponse: { res async throws in
                 #expect(res.status == .ok)
-                #expect(try res.content.decode([NoteDTO].self) == sampleTodos.map { $0.toDTO()} )
+                #expect(try res.content.decode([NoteDTO].self) == sampleTodos.map(NoteDTO.init) )
             })
         }
     }
     
     @Test("Creating a Todo")
     func createTodo() async throws {
-        let newDTO = NoteDTO(id: nil, title: "test")
+        let newDTO = NoteDTO(id: nil, name: "test")
         
         try await withApp { app in
             try await app.testing().test(.POST, "todos", beforeRequest: { req in
                 try req.content.encode(newDTO)
             }, afterResponse: { res async throws in
                 #expect(res.status == .ok)
-                let models = try await Todo.query(on: app.db).all()
-                #expect(models.map({ $0.toDTO().title }) == [newDTO.title])
+                let models = try await Note.query(on: app.db).all()
+                #expect(models.map { NoteDTO($0).name } == [newDTO.name])
             })
         }
     }
     
     @Test("Deleting a Todo")
     func deleteTodo() async throws {
-        let testTodos = [Todo(title: "test1"), Todo(title: "test2")]
+        let testTodos = [Note(title: "test1"), Note(title: "test2")]
         
         try await withApp { app in
             try await testTodos.create(on: app.db)
             
             try await app.testing().test(.DELETE, "todos/\(testTodos[0].requireID())", afterResponse: { res async throws in
                 #expect(res.status == .noContent)
-                let model = try await Todo.find(testTodos[0].id, on: app.db)
+                let model = try await Note.find(testTodos[0].id, on: app.db)
                 #expect(model == nil)
             })
         }
@@ -76,6 +76,6 @@ struct ACBackendTests {
 
 extension NoteDTO: Equatable {
     public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.id == rhs.id && lhs.title == rhs.title
+        lhs.id == rhs.id && lhs.name == rhs.name
     }
 }
